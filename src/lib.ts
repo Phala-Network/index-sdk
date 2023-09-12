d7385dc55c91801ced4c0ed508d56cc26b70092f
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2023-09-12 17:52:33 +0800

    remove buffer

--- src/lib.ts
+++ src/lib.ts
@@ -1,9 +1,13 @@
 import {Hex} from './types'
 
+function u8aToHex(u8a: Uint8Array): Hex {
+  return `0x${Array.from(u8a, (x) => x.toString(16).padStart(2, '0')).join('')}`
+}
+
 export function generateId(length = 32): Hex {
   const array = new Uint8Array(length)
   const crypto =
     typeof window === 'undefined' ? require('crypto').webcrypto : window.crypto
   crypto.getRandomValues(array)
-  return `0x${Buffer.from(array).toString('hex')}`
+  return u8aToHex(array)
 }
